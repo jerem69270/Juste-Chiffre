45c043eb1f373a508ba7b86d091e892708d01374
Jérémy <reynard.jeremy@outlook.fr>
2016-04-19 11:49:12 +0200

    Initial Commit

---
+++ Juste-Chiffre/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Juste-Chiffre
+//
+//  Created by Jérémy Reynard on 19/04/2016.
+//  Copyright © 2016 Jérémy Reynard. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
