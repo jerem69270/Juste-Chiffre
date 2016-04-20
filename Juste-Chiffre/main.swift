a79d882cb2276481a64c1603a05da07c19d7d670
Jérémy <reynard.jeremy@outlook.fr>
2016-04-20 10:00:01 +0200

    Swift code

--- Juste-Chiffre/main.swift
+++ Juste-Chiffre/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+let answer = randomIntBetween(0, high: 100)
+
+print("Enter a number between 1 and 100.")
+var turn = 1
+
+while(true) {
+    let userInput = input()
+    let inputAsInt = Int(userInput) // convert string to int , inputAsInt is int? type or "optional int"
+    if let guess = inputAsInt  { // check if inputAsInt contain a value (it's optionnal bind)
+        if(guess > answer) {
+            print("Lower!")
+        } else if(guess < answer) {
+            print("Higher!")
+        } else {
+            print("Correct! The answer was \(answer).")
+            break
+        }
+    }  else  {
+        print("Invalid input! Please enter a number.")
+        continue
+    }
+    turn = turn + 1
+}
+
+print("It took you \(turn) tries")
+
 
